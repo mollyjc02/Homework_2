691f1392525b23c32bd2f4796c029b4478677ad6
mollyjc02 <molly.catlin@emory.edu>
2025-02-17 09:14:33 -0500

    half way through class

--- submission_2/analysis/HCRIS_analysis.R
+++ submission_2/analysis/HCRIS_analysis.R
@@ -75,7 +75,7 @@ final.hcris.data <- final.hcris.data %>%
           price_denom = tot_discharges - mcare_discharges,
           price = price_num/price_denom)
 
-final.hcris <- final.hcris.data %>% ungroup() %>%
+final.hcris.2012 <- final.hcris.data %>% ungroup() %>%
   filter(price_denom>100, !is.na(price_denom), 
          price_num>0, !is.na(price_num),
          price<100000, 
@@ -84,16 +84,13 @@ final.hcris <- final.hcris.data %>% ungroup() %>%
           hrrp_payment = ifelse(is.na(hrrp_payment),0,abs(hrrp_payment)), #<<
     penalty = (hvbp_payment-hrrp_payment<0)) #<<
 
-mean.pen <- round(mean(final.hcris$price[which(final.hcris$penalty==1)]),2)
-mean.nopen <- round(mean(final.hcris$price[which(final.hcris$penalty==0)]),2)
+mean.pen <- round(mean(final.hcris.2012$price[which(final.hcris.2012$penalty==1)]),2)
+mean.nopen <- round(mean(final.hcris.2012$price[which(final.hcris.2012$penalty==0)]),2)
 print(mean.pen)
 print(mean.nopen)
 
 
 # 6)Split hospitals into quartiles based on bed size. Provide a table of the average price among treated/control groups for each quartile. 
-final.hcris.2012 <- final.hcris.data %>%
-filter(year == 2012)
-
 ## Define penalty: HVBP + HRRP < 0
 final.hcris.2012 <- final.hcris.2012 %>%
 mutate(
@@ -125,3 +122,40 @@ Q4 == 1 ~ "Q4"
 group_by(bed_quartile, penalty) %>%
 summarise(avg_price = mean(price, na.rm = TRUE), .groups = "drop") %>%
 pivot_wider(names_from = penalty, values_from = avg_price, names_prefix = "penalty_")
+
+print(quartile_summary)
+
+
+# 7)Find the average treatment effect using each of the following estimators: 
+## Nearest matching neighbor with inverse variance distance based on quartiles of bed size
+install.packages("Matching")
+install.packages("MatchIt")
+install.packages("WeightIt")
+
+colnames(final.hcris.2012)
+
+lp.covs <- final.hcris.2012 %>%
+  select(beds, mcaid_discharges, ip_charges, mcare_discharges, tot_mcare_payment) %>%
+  na.omit()
+
+lp.vars <- final.hcris.2012 %>%
+  select(price, penalty) %>% 
+  filter(!is.na(price) & !is.na(penalty))
+
+m.nn.var <- Matching::Match(Y=lp.vars$price,
+                            Tr=lp.vars$penalty,
+                            X=lp.covs,
+                            M=4,  #<<
+                            Weight=1,
+                            estimand="ATE")
+
+nrow(lp.vars)
+nrow(lp.covs)
+
+
+v.name=data.frame(new=c("Beds","Medicaid Discharges", "Inaptient Charges",
+                   "Medicare Discharges", "Medicare Payments"))
+
+## Nearest neighbor matching with Mahalanobis distance based on quartiles of bed size
+## Inverse propensity weighting, where the propensity scores are based on quartiles of bed size
+## Simple linear regression, adjusting for quartiles of bed size using dummy variables and appropriate interactions as discussed in class 
